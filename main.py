a6b96d0705eadd88915eeba835248a839a6bbc5b
Shu Sian Lyu <shusianlyu@gmail.com>
2023-06-18 22:18:04 -0700

    Remove page title except the first page

--- main.py
+++ main.py
@@ -5,11 +5,14 @@ df = pd.read_csv("topics.csv")
 pdf = FPDF(orientation="P", unit="mm", format="A4")
 
 for index, row in df.iterrows():
-    for i in range(int(row["Pages"])):
+    pdf.add_page()
+
+    pdf.set_font(family="Times", style="B", size=24)
+    pdf.set_text_color(100, 100, 100)
+    pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)
+    pdf.line(10, 21, 200, 21)
+
+    for i in range(int(row["Pages"]) - 1):
         pdf.add_page()
-        pdf.set_font(family="Times", style="B", size=24)
-        pdf.set_text_color(100, 100, 100)
-        pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)
-        pdf.line(10, 21, 200, 21)
 
 pdf.output("output.pdf")
